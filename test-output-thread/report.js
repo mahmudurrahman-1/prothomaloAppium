ebb1f99a22e44e25faace0018c758da932ef1e3a
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-03 23:35:39 +0600

    added

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"329946f0-07d2-43ab-81f8-2d82def99656","feature":"Search Feature","scenario":"Sujon search thing by google mic","start":1662226196234,"group":1,"content":"","tags":"","end":1662226232319,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
